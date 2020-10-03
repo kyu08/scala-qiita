99a037c41a24d0082da78ff1f188374a9adc38ce
kyu08 <kyu08@users.noreply.github.com>
2020-10-03 16:09:35 +0900

    Scala のプリミティブ型

--- src/main/scala/ScalaQiita/ScalaQiita.scala
+++ src/main/scala/ScalaQiita/ScalaQiita.scala
@@ -3,7 +3,10 @@ package ScalaQiita
 object ScalaQiita {
   def main(args: Array[String]): Unit ={
     println("\n-----------------------")
-    Closure.closure()
+//    Closure.closure()
+//    Closure.closure()
+    val bigger = 'asd
+    println(bigger)
     println("-----------------------\n")
   }
 }
