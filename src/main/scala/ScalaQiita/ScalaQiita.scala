1ae1897d0dff6446b98d5d97270a9515833b94ad
kyu08 <kyu08@users.noreply.github.com>
2020-10-07 17:53:07 +0900

    Scalaの制御構文

--- src/main/scala/ScalaQiita/ScalaQiita.scala
+++ src/main/scala/ScalaQiita/ScalaQiita.scala
@@ -3,7 +3,7 @@ package ScalaQiita
 object ScalaQiita {
   def main(args: Array[String]): Unit ={
     println("\n-----------------------")
-    Option.main()
+    ControlSyntax.main()
     println("-----------------------\n")
   }
 }
